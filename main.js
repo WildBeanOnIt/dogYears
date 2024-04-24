29e5672989f953b46fc43e1e70053b037abdb4c8
Julio Aguilera Jr <julioaguilera337@gmail.com>
2024-04-23 21:33:36 -0500

    finish converting dog yrs to human yrs

---
+++ main.js
@@ -0,0 +1,21 @@
+let myAge = 23; //* my current age (human)
+
+let earlyYears = 2 //* no idea what this does. I don't get the earlyYears var.
+
+earlyYears = earlyYears * 10.5; //* multiplied 10.5 by 2.
+
+let laterYears = myAge - 2; //* Subtracting from my current age.
+
+laterYears = laterYears * 4; //*my later years x the dog years.
+
+let myAgeInDogYears = earlyYears + laterYears;  //!Adding both early and late dog years to get my dog age.
+
+let myName = "Julio".toLowerCase(); //* The Method is converting my name into lowercase.
+
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`);  //* I am now putting everything together while using concatenation.
+
+// console.log(myName);
+// console.log(`my age in dog years ${myAgeInDogYears}`)
+// console.log(`later dog years ${laterYears}`)
+// console.log(`this is age minus (2) ${laterYears}`)
+// console.log(`early years ${earlyYears}`)
